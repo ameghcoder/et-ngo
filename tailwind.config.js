6ea235e9da8437f71d74c5110435fc70866679b4
ameghcoder <mintuser65@gmail.com>
2024-08-07 21:29:43 +0530

    add header and hero section, integrate tailwind in product, complete folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.{html,php}",
+    "./public/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
