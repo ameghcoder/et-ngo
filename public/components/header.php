b1da0e9ac8ddeabdfb69b321879e848e1354c461
ameghcoder <mintuser65@gmail.com>
2024-10-23 06:22:02 +0530

    convert to php and upload on server

---
+++ public/components/header.php
@@ -0,0 +1,32 @@
+<header class="w-full h-20 px-4 md:px-8 py-2 bg-white shadow-sm z-50 relative">
+    <div class="flex justify-between items-center">
+        <!-- left section -->
+        <div>
+            <img class="w-16 h-16" src="./public/assets/logo.png" alt="empowering tomorrow logo" />
+        </div>
+        <!-- right section -->
+        <div>
+            <ul id="menubar"
+                class="flex gap-8 transition-all absolute top-20 -left-full w-full p-4 flex-col bg-white border-t border-gray-100 md:top-0 md:left-0 md:w-auto md:relative md:flex-row md:border-transparent">
+                <li>
+                    <a class="text-2xl font-semibold text-gray-600 tracking-wide hover:underline" href="./">
+                        About Us
+                    </a>
+                </li>
+                <li>
+                    <a class="text-2xl font-semibold text-gray-600 tracking-wide hover:underline" href="./whatwedo">
+                        What We Do
+                    </a>
+                </li>
+                <li>
+                    <a class="text-2xl font-semibold text-gray-600 tracking-wide hover:underline" href="./contact-us">
+                        Contact Us
+                    </a>
+                </li>
+            </ul>
+            <button type="button" class="flex items-center justify-center md:hidden" id="hamburger-btn">
+                <span class="icon-setting hamburger icon-big"></span>
+            </button>
+        </div>
+    </div>
+</header>
--- public/script/script_v1.js
+++ public/script/script_v1.js
@@ -1,3 +1,7 @@
+const $ = e => document.querySelector(e);
+const $all = e => document.querySelectorAll(e);
+const $id = e => document.getElementById(e);
+
 const init = () => {
     const volunteerFormBtn = document.getElementById('volunteer-form-btn');
     const formContainer = document.querySelector(".form-container");
@@ -5,6 +9,12 @@ const init = () => {
     volunteerFormBtn.addEventListener("click", () => {
         formContainer.classList.toggle("flex");
         formContainer.classList.toggle("hidden");
+    })
+
+    // hamburger function
+    $id("hamburger-btn").addEventListener("click", () => {
+        $id("menubar").classList.toggle("-left-full");
+        $id("menubar").classList.toggle("left-0");
     })
 }
 
