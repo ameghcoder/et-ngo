7aaefa47e51886b03aa9f45a960d810b3b0b9ce0
ameghcoder <mintuser65@gmail.com>
2024-11-10 07:50:23 +0530

    Update Donate Now Button Component

--- public/components/donateNow.php
+++ public/components/donateNow.php
@@ -1,4 +1,4 @@
 <a href="https://milaap.org/fundraisers/support-empowering-tomorrow-1" id="volunteer-form-btn"
-    class="inline-flex text-white bg-green-600 text-xl border-0 px-8 py-2 focus:outline-none hover:bg-green-700 rounded-md hover:-translate-y-1 transition">
+    class="inline-flex text-white bg-green-600 text-xl border-0 px-8 py-2 focus:outline-none hover:bg-green-700 rounded-md hover:-translate-y-1 transition justify-center">
         Donate Now
 </a>
